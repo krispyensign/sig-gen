4d21752dc65c33665ce86bb698dd0cc4f61311a0
krispyensign <futralj@gmail.com>
2025-03-06 00:30:02 -0500

    Update getstuff.sh

--- getstuff.sh
+++ getstuff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt install -y gh
 wget https://github.com/alire-project/alire/releases/download/v2.0.2/alr-2.0.2-bin-x86_64-linux.zip
 unzip alr-2.0.2-bin-x86_64-linux.zip
 cp bin/alr /usr/local/bin/alr
